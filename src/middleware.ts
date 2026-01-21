a6db9bb28fbac9928a2c35b938f9e71acd054b95
fratercem-design <fratercem@gmail.com>
2026-01-20 18:49:54 -0800

    fix: add trustHost and update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,12 @@
 import { auth } from "./auth";
-
-export default auth;
-
+ 
+export default auth((req) => {
+  // req.auth is available here
+  console.log("ROUTE: ", req.nextUrl.pathname);
+  console.log("AUTH OBJECT: ", req.auth);
+});
+ 
+// Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: ["/admin/:path*", "/blog/:path*"],
 };
